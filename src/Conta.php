e1867a462400efdde7a4c18613c2fe57bbfdb4eb
marcosgoedert15 <marcosgoedert15@gmail.com>
2022-03-07 07:51:06 -0300

    adiciona function sacar, depositar, transferir

--- src/Conta.php
+++ src/Conta.php
@@ -2,7 +2,38 @@
 
 class Conta
 {
-    private string $cpfTitular;
-    private string $nomeTitular;
-    private float $saldo;
+    public string $cpfTitular;
+    public string $nomeTitular;
+    public float $saldo;
+
+    public function sacar(float $valorSaque) 
+    {
+        if ($valorSaque > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorSaque;
+    }
+
+    public function depositar(float $valorDeposito)
+    {
+        if ($valorDeposito > 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorDeposito;
+    }
+
+    public function transferir(float $valorTransferido, Conta $contaDestino): void
+    {
+        if ($valorTransferido > $this->valorTransferido) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorTransferido);
+        $contaDestino->depositar($valorTransferido);
+    }
 }
